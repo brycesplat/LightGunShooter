4fe5ff534711abf61710e718d228ed1f488330cb
Brisal <brycesplat@gmail.com>
2022-05-31 18:26:16 -0500

    Added Player Pawn

---
+++ Source/LightGunShooter/LightGunShooterPlayer.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LightGunShooterPlayer.h"
+
+// Sets default values
+ALightGunShooterPlayer::ALightGunShooterPlayer()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALightGunShooterPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALightGunShooterPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ALightGunShooterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
